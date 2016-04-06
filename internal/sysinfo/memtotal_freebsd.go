a88229aefa5dc916493f6f456fb606deaf7ae7e7
will@newrelic.com <will@newrelic.com>
2016-04-06 09:33:43 -0700

    Fix variable names

--- internal/sysinfo/memtotal_freebsd.go
+++ internal/sysinfo/memtotal_freebsd.go
@@ -9,18 +9,18 @@ func PhysicalMemoryBytes() (uint64, error) {
 	mib := []int32{6 /* CTL_HW */, 5 /* HW_PHYSMEM */}
 
 	buf := make([]byte, 8)
-	buf_len := uintptr(8)
+	bufLen := uintptr(8)
 
 	_, _, e1 := syscall.Syscall6(syscall.SYS___SYSCTL,
 		uintptr(unsafe.Pointer(&mib[0])), uintptr(len(mib)),
-		uintptr(unsafe.Pointer(&buf[0])), uintptr(unsafe.Pointer(&buf_len)),
+		uintptr(unsafe.Pointer(&buf[0])), uintptr(unsafe.Pointer(&bufLen)),
 		uintptr(0), uintptr(0))
 
 	if e1 != 0 {
 		return 0, e1
 	}
 
-	switch buf_len {
+	switch bufLen {
 	case 4:
 		return uint64(*(*uint32)(unsafe.Pointer(&buf[0]))), nil
 	case 8:
