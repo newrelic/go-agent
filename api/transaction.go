e75208670662583b427a72dc14b5c4ce9ae0b33e
will@newrelic.com <will@newrelic.com>
2016-05-12 11:56:12 -0700

    Improve transaction documentation

--- api/transaction.go
+++ api/transaction.go
@@ -4,8 +4,10 @@ import "net/http"
 
 type Transaction interface {
 	// If StartTransaction is called with a non-nil http.ResponseWriter, the
-	// Transaction itself may be used in it's place.  Doing so will allow
+	// Transaction itself may be used in its place.  Doing so will allow
 	// future instrumentation of the response code and response headers.
+	// These methods must not be called if the http.ResponseWriter parameter
+	// to StartTransaction was nil.
 	http.ResponseWriter
 
 	// End finishes the current transaction, stopping all further
@@ -16,13 +18,15 @@ type Transaction interface {
 
 	// SetName names the transaction.  Care should be taken to use a small
 	// number of names:  If too many names are used, transactions will not
-	// be grouped usefully.
+	// be grouped usefully.  This method will only work if called before
+	// End, otherwise an error will be returned.
 	SetName(name string) error
 
 	// NoticeError records an error an associates it with the Transaction. A
 	// stack trace is created for the error at the point at which this
 	// method is called.  If NoticeError is called multiple times in the
-	// same transaction, the first five errors are recorded.  This behavior
-	// is subject to potential change in the future.
+	// same transaction, the first five errors are recorded (this behavior
+	// is subject to potential change in the future).  This method will only
+	// work if called before End, otherwise an error will be returned.
 	NoticeError(err error) error
 }
