69b7cb573e56d4d04c0e788b38943d5fce75f3f2
will@newrelic.com <will@newrelic.com>
2016-05-31 12:43:34 -0700

    Update version number

--- version/version.go
+++ version/version.go
@@ -2,8 +2,8 @@ package version
 
 const (
 	major = "0"
-	minor = "2"
-	patch = "3"
+	minor = "3"
+	patch = "0"
 
 	// Version is the full string version of this Go-SDK.
 	Version = major + "." + minor + "." + patch
