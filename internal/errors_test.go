0c8e180565604045f267bfeff8c6bbf46681e2db
will@newrelic.com <will@newrelic.com>
2016-07-07 13:27:56 -0700

    Increase unit test coverage of errors.go

--- internal/errors_test.go
+++ internal/errors_test.go
@@ -2,6 +2,7 @@ package internal
 
 import (
 	"encoding/json"
+	"errors"
 	"testing"
 	"time"
 
@@ -75,6 +76,98 @@ func TestErrorTraceAttributes(t *testing.T) {
 			"request_uri":"my_request_uri"
 		}
 	]`)
+	if string(js) != expect {
+		t.Error(string(js))
+	}
+}
+
+func TestErrorsLifecycle(t *testing.T) {
+	ers := newTxnErrors(5)
+
+	when := time.Date(2014, time.November, 28, 1, 1, 0, 0, time.UTC)
+	e1 := txnErrorFromError(errors.New("hello"))
+	e2 := txnErrorFromResponseCode(400)
+	e3 := txnErrorFromPanic(errors.New("oh no panic"))
+	e4 := txnErrorFromPanic(123)
+	e5 := txnErrorFromError(errors.New("too many errors, dropped in harvest"))
+	e6 := txnErrorFromError(errors.New("too many errors, dropped in transaction"))
+	e1.when = when
+	e2.when = when
+	e3.when = when
+	e4.when = when
+	e5.when = when
+	e6.when = when
+	ers.Add(&e1)
+	ers.Add(&e2)
+	ers.Add(&e3)
+	ers.Add(&e4)
+	ers.Add(&e5)
+	ers.Add(&e6)
+
+	he := newHarvestErrors(4)
+	mergeTxnErrors(he, ers, "txnName", "requestURI", nil)
+	js, err := he.Data("agentRunID", time.Now())
+	if nil != err {
+		t.Error(err)
+	}
+	expect := compactJSONString(`
+[
+   "agentRunID",
+   [
+      [
+         1.41713646e+12,
+         "txnName",
+         "hello",
+         "*errors.errorString",
+         {
+            "stack_trace":null,
+            "agentAttributes":{},
+            "userAttributes":{},
+            "intrinsics":{},
+            "request_uri":"requestURI"
+         }
+      ],
+      [
+         1.41713646e+12,
+         "txnName",
+         "Bad Request",
+         "400",
+         {
+            "stack_trace":null,
+            "agentAttributes":{},
+            "userAttributes":{},
+            "intrinsics":{},
+            "request_uri":"requestURI"
+         }
+      ],
+      [
+         1.41713646e+12,
+         "txnName",
+         "oh no panic",
+         "panic",
+         {
+            "stack_trace":null,
+            "agentAttributes":{},
+            "userAttributes":{},
+            "intrinsics":{},
+            "request_uri":"requestURI"
+         }
+      ],
+      [
+         1.41713646e+12,
+         "txnName",
+         "123",
+         "panic",
+         {
+            "stack_trace":null,
+            "agentAttributes":{},
+            "userAttributes":{},
+            "intrinsics":{},
+            "request_uri":"requestURI"
+         }
+      ]
+   ]
+]`)
 	if string(js) != expect {
 		t.Error(string(js))
 	}
