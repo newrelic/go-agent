22a3ad107efa1e7d96433d7e0a16657e032bfcf9
will@newrelic.com <will@newrelic.com>
2017-02-24 11:16:39 -0800

    Reduce test expectation duplication

--- internal_errors_test.go
+++ internal_errors_test.go
@@ -13,68 +13,53 @@ func (e myError) Error() string { return "my msg" }
 
 func TestNoticeErrorBackground(t *testing.T) {
 	app := testApp(nil, nil, t)
-	txn := app.StartTransaction("myName", nil, nil)
+	txn := app.StartTransaction("hello", nil, nil)
 	err := txn.NoticeError(myError{})
 	if nil != err {
 		t.Error(err)
 	}
 	txn.End()
 	app.ExpectErrors(t, []internal.WantError{{
-		TxnName: "OtherTransaction/Go/myName",
+		TxnName: "OtherTransaction/Go/hello",
 		Msg:     "my msg",
 		Klass:   "newrelic.myError",
 		Caller:  "go-agent.TestNoticeErrorBackground",
 		URL:     "",
 	}})
 	app.ExpectErrorEvents(t, []internal.WantErrorEvent{{
-		TxnName: "OtherTransaction/Go/myName",
+		TxnName: "OtherTransaction/Go/hello",
 		Msg:     "my msg",
 		Klass:   "newrelic.myError",
 	}})
-	app.ExpectMetrics(t, []internal.WantMetric{
-		{Name: "OtherTransaction/Go/myName", Scope: "", Forced: true, Data: nil},
-		{Name: "OtherTransaction/all", Scope: "", Forced: true, Data: nil},
-		{Name: "Errors/all", Scope: "", Forced: true, Data: singleCount},
-		{Name: "Errors/allOther", Scope: "", Forced: true, Data: singleCount},
-		{Name: "Errors/OtherTransaction/Go/myName", Scope: "", Forced: true, Data: singleCount},
-	})
+	app.ExpectMetrics(t, backgroundErrorMetrics)
 }
 
 func TestNoticeErrorWeb(t *testing.T) {
 	app := testApp(nil, nil, t)
-	txn := app.StartTransaction("myName", nil, helloRequest)
+	txn := app.StartTransaction("hello", nil, helloRequest)
 	err := txn.NoticeError(myError{})
 	if nil != err {
 		t.Error(err)
 	}
 	txn.End()
 	app.ExpectErrors(t, []internal.WantError{{
-		TxnName: "WebTransaction/Go/myName",
+		TxnName: "WebTransaction/Go/hello",
 		Msg:     "my msg",
 		Klass:   "newrelic.myError",
 		Caller:  "go-agent.TestNoticeErrorWeb",
 		URL:     "/hello",
 	}})
 	app.ExpectErrorEvents(t, []internal.WantErrorEvent{{
-		TxnName: "WebTransaction/Go/myName",
+		TxnName: "WebTransaction/Go/hello",
 		Msg:     "my msg",
 		Klass:   "newrelic.myError",
 	}})
-	app.ExpectMetrics(t, []internal.WantMetric{
-		{Name: "WebTransaction/Go/myName", Scope: "", Forced: true, Data: nil},
-		{Name: "WebTransaction", Scope: "", Forced: true, Data: nil},
-		{Name: "HttpDispatcher", Scope: "", Forced: true, Data: nil},
-		{Name: "Apdex", Scope: "", Forced: true, Data: nil},
-		{Name: "Apdex/Go/myName", Scope: "", Forced: false, Data: nil},
-		{Name: "Errors/all", Scope: "", Forced: true, Data: singleCount},
-		{Name: "Errors/allWeb", Scope: "", Forced: true, Data: singleCount},
-		{Name: "Errors/WebTransaction/Go/myName", Scope: "", Forced: true, Data: singleCount},
-	})
+	app.ExpectMetrics(t, webErrorMetrics)
 }
 
 func TestNoticeErrorTxnEnded(t *testing.T) {
 	app := testApp(nil, nil, t)
-	txn := app.StartTransaction("myName", nil, nil)
+	txn := app.StartTransaction("hello", nil, nil)
 	txn.End()
 	err := txn.NoticeError(myError{})
 	if err != errAlreadyEnded {
@@ -83,45 +68,36 @@ func TestNoticeErrorTxnEnded(t *testing.T) {
 	txn.End()
 	app.ExpectErrors(t, []internal.WantError{})
 	app.ExpectErrorEvents(t, []internal.WantErrorEvent{})
-	app.ExpectMetrics(t, []internal.WantMetric{
-		{Name: "OtherTransaction/Go/myName", Scope: "", Forced: true, Data: nil},
-		{Name: "OtherTransaction/all", Scope: "", Forced: true, Data: nil},
-	})
+	app.ExpectMetrics(t, backgroundMetrics)
 }
 
 func TestNoticeErrorHighSecurity(t *testing.T) {
 	cfgFn := func(cfg *Config) { cfg.HighSecurity = true }
 	app := testApp(nil, cfgFn, t)
-	txn := app.StartTransaction("myName", nil, nil)
+	txn := app.StartTransaction("hello", nil, nil)
 	err := txn.NoticeError(myError{})
 	if nil != err {
 		t.Error(err)
 	}
 	txn.End()
 	app.ExpectErrors(t, []internal.WantError{{
-		TxnName: "OtherTransaction/Go/myName",
+		TxnName: "OtherTransaction/Go/hello",
 		Msg:     highSecurityErrorMsg,
 		Klass:   "newrelic.myError",
 		Caller:  "go-agent.TestNoticeErrorHighSecurity",
 	}})
 	app.ExpectErrorEvents(t, []internal.WantErrorEvent{{
-		TxnName: "OtherTransaction/Go/myName",
+		TxnName: "OtherTransaction/Go/hello",
 		Msg:     highSecurityErrorMsg,
 		Klass:   "newrelic.myError",
 	}})
-	app.ExpectMetrics(t, []internal.WantMetric{
-		{Name: "OtherTransaction/Go/myName", Scope: "", Forced: true, Data: nil},
-		{Name: "OtherTransaction/all", Scope: "", Forced: true, Data: nil},
-		{Name: "Errors/all", Scope: "", Forced: true, Data: singleCount},
-		{Name: "Errors/allOther", Scope: "", Forced: true, Data: singleCount},
-		{Name: "Errors/OtherTransaction/Go/myName", Scope: "", Forced: true, Data: singleCount},
-	})
+	app.ExpectMetrics(t, backgroundErrorMetrics)
 }
 
 func TestNoticeErrorLocallyDisabled(t *testing.T) {
 	cfgFn := func(cfg *Config) { cfg.ErrorCollector.Enabled = false }
 	app := testApp(nil, cfgFn, t)
-	txn := app.StartTransaction("myName", nil, nil)
+	txn := app.StartTransaction("hello", nil, nil)
 	err := txn.NoticeError(myError{})
 	if errorsLocallyDisabled != err {
 		t.Error(err)
@@ -129,16 +105,13 @@ func TestNoticeErrorLocallyDisabled(t *testing.T) {
 	txn.End()
 	app.ExpectErrors(t, []internal.WantError{})
 	app.ExpectErrorEvents(t, []internal.WantErrorEvent{})
-	app.ExpectMetrics(t, []internal.WantMetric{
-		{Name: "OtherTransaction/Go/myName", Scope: "", Forced: true, Data: nil},
-		{Name: "OtherTransaction/all", Scope: "", Forced: true, Data: nil},
-	})
+	app.ExpectMetrics(t, backgroundMetrics)
 }
 
 func TestNoticeErrorRemotelyDisabled(t *testing.T) {
 	replyfn := func(reply *internal.ConnectReply) { reply.CollectErrors = false }
 	app := testApp(replyfn, nil, t)
-	txn := app.StartTransaction("myName", nil, nil)
+	txn := app.StartTransaction("hello", nil, nil)
 	err := txn.NoticeError(myError{})
 	if errorsRemotelyDisabled != err {
 		t.Error(err)
@@ -146,15 +119,12 @@ func TestNoticeErrorRemotelyDisabled(t *testing.T) {
 	txn.End()
 	app.ExpectErrors(t, []internal.WantError{})
 	app.ExpectErrorEvents(t, []internal.WantErrorEvent{})
-	app.ExpectMetrics(t, []internal.WantMetric{
-		{Name: "OtherTransaction/Go/myName", Scope: "", Forced: true, Data: nil},
-		{Name: "OtherTransaction/all", Scope: "", Forced: true, Data: nil},
-	})
+	app.ExpectMetrics(t, backgroundMetrics)
 }
 
 func TestNoticeErrorNil(t *testing.T) {
 	app := testApp(nil, nil, t)
-	txn := app.StartTransaction("myName", nil, nil)
+	txn := app.StartTransaction("hello", nil, nil)
 	err := txn.NoticeError(nil)
 	if errNilError != err {
 		t.Error(err)
@@ -162,60 +132,45 @@ func TestNoticeErrorNil(t *testing.T) {
 	txn.End()
 	app.ExpectErrors(t, []internal.WantError{})
 	app.ExpectErrorEvents(t, []internal.WantErrorEvent{})
-	app.ExpectMetrics(t, []internal.WantMetric{
-		{Name: "OtherTransaction/Go/myName", Scope: "", Forced: true, Data: nil},
-		{Name: "OtherTransaction/all", Scope: "", Forced: true, Data: nil},
-	})
+	app.ExpectMetrics(t, backgroundMetrics)
 }
 
 func TestNoticeErrorEventsLocallyDisabled(t *testing.T) {
 	cfgFn := func(cfg *Config) { cfg.ErrorCollector.CaptureEvents = false }
 	app := testApp(nil, cfgFn, t)
-	txn := app.StartTransaction("myName", nil, nil)
+	txn := app.StartTransaction("hello", nil, nil)
 	err := txn.NoticeError(myError{})
 	if nil != err {
 		t.Error(err)
 	}
 	txn.End()
 	app.ExpectErrors(t, []internal.WantError{{
-		TxnName: "OtherTransaction/Go/myName",
+		TxnName: "OtherTransaction/Go/hello",
 		Msg:     "my msg",
 		Klass:   "newrelic.myError",
 		Caller:  "go-agent.TestNoticeErrorEventsLocallyDisabled",
 	}})
 	app.ExpectErrorEvents(t, []internal.WantErrorEvent{})
-	app.ExpectMetrics(t, []internal.WantMetric{
-		{Name: "OtherTransaction/Go/myName", Scope: "", Forced: true, Data: nil},
-		{Name: "OtherTransaction/all", Scope: "", Forced: true, Data: nil},
-		{Name: "Errors/all", Scope: "", Forced: true, Data: singleCount},
-		{Name: "Errors/allOther", Scope: "", Forced: true, Data: singleCount},
-		{Name: "Errors/OtherTransaction/Go/myName", Scope: "", Forced: true, Data: singleCount},
-	})
+	app.ExpectMetrics(t, backgroundErrorMetrics)
 }
 
 func TestNoticeErrorEventsRemotelyDisabled(t *testing.T) {
 	replyfn := func(reply *internal.ConnectReply) { reply.CollectErrorEvents = false }
 	app := testApp(replyfn, nil, t)
-	txn := app.StartTransaction("myName", nil, nil)
+	txn := app.StartTransaction("hello", nil, nil)
 	err := txn.NoticeError(myError{})
 	if nil != err {
 		t.Error(err)
 	}
 	txn.End()
 	app.ExpectErrors(t, []internal.WantError{{
-		TxnName: "OtherTransaction/Go/myName",
+		TxnName: "OtherTransaction/Go/hello",
 		Msg:     "my msg",
 		Klass:   "newrelic.myError",
 		Caller:  "go-agent.TestNoticeErrorEventsRemotelyDisabled",
 	}})
 	app.ExpectErrorEvents(t, []internal.WantErrorEvent{})
-	app.ExpectMetrics(t, []internal.WantMetric{
-		{Name: "OtherTransaction/Go/myName", Scope: "", Forced: true, Data: nil},
-		{Name: "OtherTransaction/all", Scope: "", Forced: true, Data: nil},
-		{Name: "Errors/all", Scope: "", Forced: true, Data: singleCount},
-		{Name: "Errors/allOther", Scope: "", Forced: true, Data: singleCount},
-		{Name: "Errors/OtherTransaction/Go/myName", Scope: "", Forced: true, Data: singleCount},
-	})
+	app.ExpectMetrics(t, backgroundErrorMetrics)
 }
 
 type errorWithClass struct{ class string }
@@ -225,60 +180,48 @@ func (e errorWithClass) ErrorClass() string { return e.class }
 
 func TestErrorWithClasser(t *testing.T) {
 	app := testApp(nil, nil, t)
-	txn := app.StartTransaction("myName", nil, nil)
+	txn := app.StartTransaction("hello", nil, nil)
 	err := txn.NoticeError(errorWithClass{class: "zap"})
 	if nil != err {
 		t.Error(err)
 	}
 	txn.End()
 	app.ExpectErrors(t, []internal.WantError{{
-		TxnName: "OtherTransaction/Go/myName",
+		TxnName: "OtherTransaction/Go/hello",
 		Msg:     "my msg",
 		Klass:   "zap",
 		Caller:  "go-agent.TestErrorWithClasser",
 		URL:     "",
 	}})
 	app.ExpectErrorEvents(t, []internal.WantErrorEvent{{
-		TxnName: "OtherTransaction/Go/myName",
+		TxnName: "OtherTransaction/Go/hello",
 		Msg:     "my msg",
 		Klass:   "zap",
 	}})
-	app.ExpectMetrics(t, []internal.WantMetric{
-		{Name: "OtherTransaction/Go/myName", Scope: "", Forced: true, Data: nil},
-		{Name: "OtherTransaction/all", Scope: "", Forced: true, Data: nil},
-		{Name: "Errors/all", Scope: "", Forced: true, Data: singleCount},
-		{Name: "Errors/allOther", Scope: "", Forced: true, Data: singleCount},
-		{Name: "Errors/OtherTransaction/Go/myName", Scope: "", Forced: true, Data: singleCount},
-	})
+	app.ExpectMetrics(t, backgroundErrorMetrics)
 }
 
 func TestErrorWithClasserReturnsEmpty(t *testing.T) {
 	app := testApp(nil, nil, t)
-	txn := app.StartTransaction("myName", nil, nil)
+	txn := app.StartTransaction("hello", nil, nil)
 	err := txn.NoticeError(errorWithClass{class: ""})
 	if nil != err {
 		t.Error(err)
 	}
 	txn.End()
 	app.ExpectErrors(t, []internal.WantError{{
-		TxnName: "OtherTransaction/Go/myName",
+		TxnName: "OtherTransaction/Go/hello",
 		Msg:     "my msg",
 		Klass:   "newrelic.errorWithClass",
 		Caller:  "go-agent.TestErrorWithClasserReturnsEmpty",
 		URL:     "",
 	}})
 	app.ExpectErrorEvents(t, []internal.WantErrorEvent{{
-		TxnName: "OtherTransaction/Go/myName",
+		TxnName: "OtherTransaction/Go/hello",
 		Msg:     "my msg",
 		Klass:   "newrelic.errorWithClass",
 	}})
-	app.ExpectMetrics(t, []internal.WantMetric{
-		{Name: "OtherTransaction/Go/myName", Scope: "", Forced: true, Data: nil},
-		{Name: "OtherTransaction/all", Scope: "", Forced: true, Data: nil},
-		{Name: "Errors/all", Scope: "", Forced: true, Data: singleCount},
-		{Name: "Errors/allOther", Scope: "", Forced: true, Data: singleCount},
-		{Name: "Errors/OtherTransaction/Go/myName", Scope: "", Forced: true, Data: singleCount},
-	})
+	app.ExpectMetrics(t, backgroundErrorMetrics)
 }
 
 type withStackTrace struct{ trace []uintptr }
@@ -296,7 +239,7 @@ func (e withStackTrace) StackTrace() []uintptr { return e.trace }
 
 func TestErrorWithStackTrace(t *testing.T) {
 	app := testApp(nil, nil, t)
-	txn := app.StartTransaction("myName", nil, nil)
+	txn := app.StartTransaction("hello", nil, nil)
 	e := makeErrorWithStackTrace()
 	err := txn.NoticeError(e)
 	if nil != err {
@@ -304,29 +247,23 @@ func TestErrorWithStackTrace(t *testing.T) {
 	}
 	txn.End()
 	app.ExpectErrors(t, []internal.WantError{{
-		TxnName: "OtherTransaction/Go/myName",
+		TxnName: "OtherTransaction/Go/hello",
 		Msg:     "my msg",
 		Klass:   "newrelic.withStackTrace",
 		Caller:  "go-agent.makeErrorWithStackTrace",
 		URL:     "",
 	}})
 	app.ExpectErrorEvents(t, []internal.WantErrorEvent{{
-		TxnName: "OtherTransaction/Go/myName",
+		TxnName: "OtherTransaction/Go/hello",
 		Msg:     "my msg",
 		Klass:   "newrelic.withStackTrace",
 	}})
-	app.ExpectMetrics(t, []internal.WantMetric{
-		{Name: "OtherTransaction/Go/myName", Scope: "", Forced: true, Data: nil},
-		{Name: "OtherTransaction/all", Scope: "", Forced: true, Data: nil},
-		{Name: "Errors/all", Scope: "", Forced: true, Data: singleCount},
-		{Name: "Errors/allOther", Scope: "", Forced: true, Data: singleCount},
-		{Name: "Errors/OtherTransaction/Go/myName", Scope: "", Forced: true, Data: singleCount},
-	})
+	app.ExpectMetrics(t, backgroundErrorMetrics)
 }
 
 func TestErrorWithStackTraceReturnsNil(t *testing.T) {
 	app := testApp(nil, nil, t)
-	txn := app.StartTransaction("myName", nil, nil)
+	txn := app.StartTransaction("hello", nil, nil)
 	e := withStackTrace{trace: nil}
 	err := txn.NoticeError(e)
 	if nil != err {
@@ -334,22 +271,16 @@ func TestErrorWithStackTraceReturnsNil(t *testing.T) {
 	}
 	txn.End()
 	app.ExpectErrors(t, []internal.WantError{{
-		TxnName: "OtherTransaction/Go/myName",
+		TxnName: "OtherTransaction/Go/hello",
 		Msg:     "my msg",
 		Klass:   "newrelic.withStackTrace",
 		Caller:  "go-agent.TestErrorWithStackTraceReturnsNil",
 		URL:     "",
 	}})
 	app.ExpectErrorEvents(t, []internal.WantErrorEvent{{
-		TxnName: "OtherTransaction/Go/myName",
+		TxnName: "OtherTransaction/Go/hello",
 		Msg:     "my msg",
 		Klass:   "newrelic.withStackTrace",
 	}})
-	app.ExpectMetrics(t, []internal.WantMetric{
-		{Name: "OtherTransaction/Go/myName", Scope: "", Forced: true, Data: nil},
-		{Name: "OtherTransaction/all", Scope: "", Forced: true, Data: nil},
-		{Name: "Errors/all", Scope: "", Forced: true, Data: singleCount},
-		{Name: "Errors/allOther", Scope: "", Forced: true, Data: singleCount},
-		{Name: "Errors/OtherTransaction/Go/myName", Scope: "", Forced: true, Data: singleCount},
-	})
+	app.ExpectMetrics(t, backgroundErrorMetrics)
 }
