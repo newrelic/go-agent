ec32fdcb086fae220871c9ee69d75d8619613ccc
Robert Lewis <rlewis@newrelic.com>
2017-10-20 09:24:05 -0700

    Formatting

--- internal/sysinfo/errors.go
+++ internal/sysinfo/errors.go
@@ -1,10 +1,10 @@
 package sysinfo
 
 import (
-  "errors"
+	"errors"
 )
 
 var (
-  ErrFeatureUnsupported = errors.New("That feature is not supported on this platform")
-  ErrIdentifierNotFound = errors.New("The requested identifier could not be found")
+	ErrFeatureUnsupported = errors.New("That feature is not supported on this platform")
+	ErrIdentifierNotFound = errors.New("The requested identifier could not be found")
 )
