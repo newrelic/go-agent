aaa7ff0e7fb471eebe8c6f98d8df191b256ad18c
Rey Abolofia <rabolofia@newrelic.com>
2019-08-12 15:40:05 -0700

    Instrument Micro Clients.

---
+++ _integrations/nrmicro/nrmicro.go
@@ -0,0 +1,37 @@
+package nrmicro
+
+import (
+	"context"
+
+	"github.com/micro/go-micro/client"
+	"github.com/micro/go-micro/metadata"
+	newrelic "github.com/newrelic/go-agent"
+)
+
+type nrWrapper struct {
+	client.Client
+}
+
+func (n *nrWrapper) Call(ctx context.Context, req client.Request, rsp interface{}, opts ...client.CallOption) error {
+	if txn := newrelic.FromContext(ctx); nil != txn {
+		seg := newrelic.ExternalSegment{
+			StartTime: newrelic.StartSegmentNow(txn),
+			Procedure: req.Endpoint(),
+			Library:   "Micro",
+			Host:      req.Service(),
+		}
+		defer seg.End()
+		payload := txn.CreateDistributedTracePayload()
+		if txt := payload.Text(); "" != txt {
+			md, _ := metadata.FromContext(ctx)
+			md = metadata.Copy(md)
+			md[newrelic.DistributedTracePayloadHeader] = txt
+			ctx = metadata.NewContext(ctx, md)
+		}
+	}
+	return n.Client.Call(ctx, req, rsp, opts...)
+}
+
+func ClientWrapper(c client.Client) client.Client {
+	return &nrWrapper{c}
+}
