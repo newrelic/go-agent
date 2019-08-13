94ce740da8f18c028e87b7244b077c6f1622440d
Rey Abolofia <rabolofia@newrelic.com>
2019-08-13 12:53:51 -0700

    Test Client Publish.

--- _integrations/nrmicro/nrmicro_test.go
+++ _integrations/nrmicro/nrmicro_test.go
@@ -2,12 +2,16 @@ package nrmicro
 
 import (
 	"context"
+	"sync"
 	"testing"
+	"time"
 
+	"github.com/micro/go-micro/broker"
+	bmemory "github.com/micro/go-micro/broker/memory"
 	"github.com/micro/go-micro/client"
 	"github.com/micro/go-micro/client/selector"
 	"github.com/micro/go-micro/metadata"
-	"github.com/micro/go-micro/registry/memory"
+	rmemory "github.com/micro/go-micro/registry/memory"
 	"github.com/micro/go-micro/server"
 	newrelic "github.com/newrelic/go-agent"
 	"github.com/newrelic/go-agent/internal"
@@ -65,7 +69,7 @@ func createTestApp(t *testing.T) newrelic.Application {
 }
 
 func newTestClientAndServer(t *testing.T) (client.Client, server.Server) {
-	registry := memory.NewRegistry()
+	registry := rmemory.NewRegistry()
 	sel := selector.NewSelector(selector.Registry(registry))
 	c := client.NewClient(
 		client.Selector(sel),
@@ -188,4 +192,136 @@ func TestClientCallMetadata(t *testing.T) {
 	if len(md) != 1 || md["zip"] != "zap" {
 		t.Error("metadata changed:", md)
 	}
+}
+
+func newTestClientAndBroker(t *testing.T) (client.Client, broker.Broker) {
+	b := bmemory.NewBroker()
+	c := client.NewClient(
+		client.Broker(b),
+		client.Wrap(ClientWrapper),
+	)
+	return c, b
+}
+
+func waitOrTimeout(t *testing.T, wg sync.WaitGroup) {
+	ch := make(chan struct{})
+	go func() {
+		defer close(ch)
+		wg.Wait()
+	}()
+	select {
+	case <-ch:
+	case <-time.After(time.Second):
+		t.Fatal("timeout waiting for message")
+	}
+}
+
+var topic = "topic"
+
+func TestClientPublishWithNoTransaction(t *testing.T) {
+	c, b := newTestClientAndBroker(t)
+
+	var wg sync.WaitGroup
+	if err := b.Connect(); nil != err {
+		t.Fatal("broker connect error:", err)
+	}
+	defer b.Disconnect()
+	if _, err := b.Subscribe(topic, func(e broker.Event) error {
+		defer wg.Done()
+		h := e.Message().Header
+		if _, ok := h[newrelic.DistributedTracePayloadHeader]; ok {
+			t.Error("Distributed tracing headers found", h)
+		}
+		return nil
+	}); nil != err {
+		t.Fatal("Failure to subscribe to broker:", err)
+	}
+
+	ctx := context.Background()
+	msg := c.NewMessage(topic, "hello world")
+	wg.Add(1)
+	if err := c.Publish(ctx, msg); nil != err {
+		t.Fatal("Error calling test client:", err)
+	}
+	waitOrTimeout(t, wg)
+}
+
+func TestClientPublishWithTransaction(t *testing.T) {
+	c, b := newTestClientAndBroker(t)
+
+	var wg sync.WaitGroup
+	if err := b.Connect(); nil != err {
+		t.Fatal("broker connect error:", err)
+	}
+	defer b.Disconnect()
+	if _, err := b.Subscribe(topic, func(e broker.Event) error {
+		defer wg.Done()
+		h := e.Message().Header
+		if _, ok := h[newrelic.DistributedTracePayloadHeader]; !ok {
+			t.Error("Distributed tracing headers not found", h)
+		}
+		return nil
+	}); nil != err {
+		t.Fatal("Failure to subscribe to broker:", err)
+	}
+
+	app := createTestApp(t)
+	txn := app.StartTransaction("name", nil, nil)
+	ctx := newrelic.NewContext(context.Background(), txn)
+	msg := c.NewMessage(topic, "hello world")
+	wg.Add(1)
+	if err := c.Publish(ctx, msg); nil != err {
+		t.Fatal("Error calling test client:", err)
+	}
+	waitOrTimeout(t, wg)
+
+	txn.End()
+	app.(internal.Expect).ExpectMetrics(t, []internal.WantMetric{
+		{Name: "Custom/Publish", Scope: "", Forced: false, Data: nil},
+		{Name: "Custom/Publish", Scope: "OtherTransaction/Go/name", Forced: false, Data: nil},
+		{Name: "DurationByCaller/Unknown/Unknown/Unknown/Unknown/all", Scope: "", Forced: false, Data: nil},
+		{Name: "DurationByCaller/Unknown/Unknown/Unknown/Unknown/allOther", Scope: "", Forced: false, Data: nil},
+		{Name: "OtherTransaction/Go/name", Scope: "", Forced: true, Data: nil},
+		{Name: "OtherTransaction/all", Scope: "", Forced: true, Data: nil},
+		{Name: "OtherTransactionTotalTime", Scope: "", Forced: true, Data: nil},
+		{Name: "OtherTransactionTotalTime/Go/name", Scope: "", Forced: false, Data: nil},
+		{Name: "Supportability/DistributedTrace/CreatePayload/Success", Scope: "", Forced: true, Data: nil},
+	})
+	app.(internal.Expect).ExpectSpanEvents(t, []internal.WantEvent{
+		{
+			Intrinsics: map[string]interface{}{
+				"category":      "generic",
+				"name":          "OtherTransaction/Go/name",
+				"nr.entryPoint": true,
+			},
+			UserAttributes:  map[string]interface{}{},
+			AgentAttributes: map[string]interface{}{},
+		},
+		{
+			Intrinsics: map[string]interface{}{
+				"category": "generic",
+				"name":     "Custom/Publish",
+				"parentId": internal.MatchAnything,
+			},
+			UserAttributes:  map[string]interface{}{},
+			AgentAttributes: map[string]interface{}{},
+		},
+	})
+	app.(internal.Expect).ExpectTxnTraces(t, []internal.WantTxnTrace{{
+		MetricName: "OtherTransaction/Go/name",
+		Root: internal.WantTraceSegment{
+			SegmentName: "ROOT",
+			Attributes:  map[string]interface{}{},
+			Children: []internal.WantTraceSegment{{
+				SegmentName: "OtherTransaction/Go/name",
+				Attributes:  map[string]interface{}{"exclusive_duration_millis": internal.MatchAnything},
+				Children: []internal.WantTraceSegment{
+					{
+						SegmentName: "Custom/Publish",
+						Attributes:  map[string]interface{}{},
+					},
+				},
+			}},
+		},
+	}})
 }
