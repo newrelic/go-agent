2fdcb9ccdd74d7fe80a11725bba9afc7b9a0e2e3
Rey Abolofia <rabolofia@newrelic.com>
2019-08-30 16:04:10 -0700

    Demonstrate how API can be used with Stan.

--- _integrations/nrnats/example_test.go
+++ _integrations/nrnats/example_test.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/nats-io/nats.go"
+	"github.com/nats-io/stan.go"
 	newrelic "github.com/newrelic/go-agent"
 )
 
@@ -37,4 +38,19 @@ func ExampleStartPublishSegment_defer() {
 		panic(err)
 	}
 	fmt.Println("Received reply message:", string(m.Data))
+}
+
+var clusterID, clientID string
+
+// StartPublishSegment can be used with a NATS Streamming Connection as well
+// (https://github.com/nats-io/stan.go).  Use the `NatsConn()` method on the
+// `stan.Conn` interface (https://godoc.org/github.com/nats-io/stan#Conn) to
+// access the `nats.Conn` object.
+func ExampleStartPublishSegment_stan() {
+	sc, _ := stan.Connect(clusterID, clientID)
+	txn := currentTransaction()
+	subject := "testing.subject"
+
+	defer StartPublishSegment(txn, sc.NatsConn(), subject).End()
+	sc.Publish(subject, []byte("Hello World"))
 }
