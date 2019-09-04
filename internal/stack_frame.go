21aca54fb777ae55f0b618ae5bbb7c99c15d0f6f
eheinlein <eheinlein@newrelic.com>
2019-09-04 11:27:54 -0700

    Add comment

--- internal/stack_frame.go
+++ internal/stack_frame.go
@@ -6,7 +6,7 @@ import "runtime"
 
 func (st StackTrace) frames() []stacktraceFrame {
 	fs := make([]stacktraceFrame, maxStackTraceFrames)
-	frames := runtime.CallersFrames(st)
+	frames := runtime.CallersFrames(st) // CallersFrames is only available in Go 1.7+
 	i := 0
 	for frame, more := frames.Next(); more && (i < maxStackTraceFrames); frame, more = frames.Next() {
 		fs[i] = stacktraceFrame{
