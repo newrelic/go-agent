b287d442e0b3fc81ade97227d3866b4cde02a56c
eheinlein <eheinlein@newrelic.com>
2019-10-24 13:54:56 -0700

    Bump version for release

--- version.go
+++ version.go
@@ -8,8 +8,8 @@ import (
 
 const (
 	major = "2"
-	minor = "14"
-	patch = "1"
+	minor = "15"
+	patch = "0"
 
 	// Version is the full string version of this Go Agent.
 	Version = major + "." + minor + "." + patch
