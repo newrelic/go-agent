f79c62ca9b5cb3cf55f92f0b910216fba3d808c8
Rey Abolofia <rabolofia@newrelic.com>
2019-11-20 09:29:56 -0800

    Replace whitelist with allowlist throughout code.

--- v3/internal/segment_terms.go
+++ v3/internal/segment_terms.go
@@ -81,8 +81,8 @@ func (rule *segmentRule) apply(name string) string {
 		segments := strings.Split(s, separator)
 
 		for i, segment := range segments {
-			_, whitelisted := rule.TermsMap[segment]
-			if whitelisted {
+			_, allowed := rule.TermsMap[segment]
+			if allowed {
 				segments[i] = segment
 			} else {
 				segments[i] = placeholder
