e851d920d9771f35ca4f00095b2a72fbd052f755
will@newrelic.com <will@newrelic.com>
2019-12-16 14:05:55 -0800

    Add package doc comments

---
+++ v3/newrelic/doc.go
@@ -0,0 +1,9 @@
+// Package newrelic provides instrumentation for Go applications.
+//
+// Additional documention available here:
+//
+// https://github.com/newrelic/go-agent/blob/master/GETTING_STARTED.md
+//
+// https://github.com/newrelic/go-agent/blob/master/GUIDE.md
+//
+package newrelic
