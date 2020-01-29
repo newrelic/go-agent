29b07eb31c65ff08566ce11de4c662bab2650114
Rey Abolofia <rabolofia@newrelic.com>
2020-01-29 15:52:47 -0800

    Test errors during resolve.

--- v3/integrations/nr-graphql-go/nrgraphql_test.go
+++ v3/integrations/nr-graphql-go/nrgraphql_test.go
@@ -3,6 +3,7 @@ package nrgraphql
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"testing"
 
 	"github.com/graphql-go/graphql"
@@ -16,7 +17,20 @@ var schema = func() graphql.Schema {
 		Query: graphql.NewObject(graphql.ObjectConfig{
 			Name: "RootQuery",
 			Fields: graphql.Fields{
-				"hello": &graphql.Field{
+				// fields will get executed alphabetically
+				"a_hello": &graphql.Field{
+					Type: graphql.String,
+					Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+						return "world", nil
+					},
+				},
+				"b_errors": &graphql.Field{
+					Type: graphql.String,
+					Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+						return nil, errors.New("ooooooops")
+					},
+				},
+				"c_goodbye": &graphql.Field{
 					Type: graphql.String,
 					Resolve: func(p graphql.ResolveParams) (interface{}, error) {
 						return "world", nil
@@ -33,7 +47,7 @@ var schema = func() graphql.Schema {
 }()
 
 func TestExtensionNoTransaction(t *testing.T) {
-	query := `{ hello }`
+	query := `{ a_hello }`
 	params := graphql.Params{Schema: schema, RequestString: query}
 	resp := graphql.Do(params)
 	for _, err := range resp.Errors {
@@ -43,7 +57,7 @@ func TestExtensionNoTransaction(t *testing.T) {
 	if err != nil {
 		t.Error("failure to marshal json:", err)
 	}
-	if data := string(js); data != `{"hello":"world"}` {
+	if data := string(js); data != `{"a_hello":"world"}` {
 		t.Error("incorrect response data:", data)
 	}
 }
@@ -53,7 +67,7 @@ func TestExtensionWithTransaction(t *testing.T) {
 	txn := app.StartTransaction("query")
 	ctx := newrelic.NewContext(context.Background(), txn)
 
-	query := `{ hello }`
+	query := `{ a_hello }`
 	params := graphql.Params{
 		Schema:        schema,
 		RequestString: query,
@@ -67,7 +81,7 @@ func TestExtensionWithTransaction(t *testing.T) {
 	if err != nil {
 		t.Error("failure to marshal json:", err)
 	}
-	if data := string(js); data != `{"hello":"world"}` {
+	if data := string(js); data != `{"a_hello":"world"}` {
 		t.Error("incorrect response data:", data)
 	}
 
@@ -77,8 +91,92 @@ func TestExtensionWithTransaction(t *testing.T) {
 		{Name: "Custom/Execution", Scope: "OtherTransaction/Go/query", Forced: false, Data: nil},
 		{Name: "Custom/Parse", Scope: "", Forced: false, Data: nil},
 		{Name: "Custom/Parse", Scope: "OtherTransaction/Go/query", Forced: false, Data: nil},
-		{Name: "Custom/Resolve hello", Scope: "", Forced: false, Data: nil},
-		{Name: "Custom/Resolve hello", Scope: "OtherTransaction/Go/query", Forced: false, Data: nil},
+		{Name: "Custom/Resolve a_hello", Scope: "", Forced: false, Data: nil},
+		{Name: "Custom/Resolve a_hello", Scope: "OtherTransaction/Go/query", Forced: false, Data: nil},
+		{Name: "Custom/Validation", Scope: "", Forced: false, Data: nil},
+		{Name: "Custom/Validation", Scope: "OtherTransaction/Go/query", Forced: false, Data: nil},
+		{Name: "OtherTransaction/Go/query", Scope: "", Forced: true, Data: nil},
+		{Name: "OtherTransaction/all", Scope: "", Forced: true, Data: nil},
+		{Name: "OtherTransactionTotalTime", Scope: "", Forced: true, Data: nil},
+		{Name: "OtherTransactionTotalTime/Go/query", Scope: "", Forced: false, Data: nil},
+	})
+}
+
+func TestExtensionResolveErrorFirst(t *testing.T) {
+	app := integrationsupport.NewBasicTestApp()
+	txn := app.StartTransaction("query")
+	ctx := newrelic.NewContext(context.Background(), txn)
+
+	query := `{ b_errors c_goodbye }`
+	params := graphql.Params{
+		Schema:        schema,
+		RequestString: query,
+		Context:       ctx,
+	}
+	resp := graphql.Do(params)
+	if len(resp.Errors) != 1 {
+		t.Error("incorrect number of errors on response", resp.Errors)
+	}
+	js, err := json.Marshal(resp.Data)
+	if err != nil {
+		t.Error("failure to marshal json:", err)
+	}
+	if data := string(js); data != `{"b_errors":null,"c_goodbye":"world"}` {
+		t.Error("incorrect response data:", data)
+	}
+
+	txn.End()
+	app.ExpectMetrics(t, []internal.WantMetric{
+		{Name: "Custom/Execution", Scope: "", Forced: false, Data: nil},
+		{Name: "Custom/Execution", Scope: "OtherTransaction/Go/query", Forced: false, Data: nil},
+		{Name: "Custom/Parse", Scope: "", Forced: false, Data: nil},
+		{Name: "Custom/Parse", Scope: "OtherTransaction/Go/query", Forced: false, Data: nil},
+		{Name: "Custom/Resolve b_errors", Scope: "", Forced: false, Data: nil},
+		{Name: "Custom/Resolve b_errors", Scope: "OtherTransaction/Go/query", Forced: false, Data: nil},
+		{Name: "Custom/Resolve c_goodbye", Scope: "", Forced: false, Data: nil},
+		{Name: "Custom/Resolve c_goodbye", Scope: "OtherTransaction/Go/query", Forced: false, Data: nil},
+		{Name: "Custom/Validation", Scope: "", Forced: false, Data: nil},
+		{Name: "Custom/Validation", Scope: "OtherTransaction/Go/query", Forced: false, Data: nil},
+		{Name: "OtherTransaction/Go/query", Scope: "", Forced: true, Data: nil},
+		{Name: "OtherTransaction/all", Scope: "", Forced: true, Data: nil},
+		{Name: "OtherTransactionTotalTime", Scope: "", Forced: true, Data: nil},
+		{Name: "OtherTransactionTotalTime/Go/query", Scope: "", Forced: false, Data: nil},
+	})
+}
+
+func TestExtensionResolveErrorLast(t *testing.T) {
+	app := integrationsupport.NewBasicTestApp()
+	txn := app.StartTransaction("query")
+	ctx := newrelic.NewContext(context.Background(), txn)
+
+	query := `{ a_hello b_errors }`
+	params := graphql.Params{
+		Schema:        schema,
+		RequestString: query,
+		Context:       ctx,
+	}
+	resp := graphql.Do(params)
+	if len(resp.Errors) != 1 {
+		t.Error("incorrect number of errors on response", resp.Errors)
+	}
+	js, err := json.Marshal(resp.Data)
+	if err != nil {
+		t.Error("failure to marshal json:", err)
+	}
+	if data := string(js); data != `{"a_hello":"world","b_errors":null}` {
+		t.Error("incorrect response data:", data)
+	}
+
+	txn.End()
+	app.ExpectMetrics(t, []internal.WantMetric{
+		{Name: "Custom/Execution", Scope: "", Forced: false, Data: nil},
+		{Name: "Custom/Execution", Scope: "OtherTransaction/Go/query", Forced: false, Data: nil},
+		{Name: "Custom/Parse", Scope: "", Forced: false, Data: nil},
+		{Name: "Custom/Parse", Scope: "OtherTransaction/Go/query", Forced: false, Data: nil},
+		{Name: "Custom/Resolve a_hello", Scope: "", Forced: false, Data: nil},
+		{Name: "Custom/Resolve a_hello", Scope: "OtherTransaction/Go/query", Forced: false, Data: nil},
+		{Name: "Custom/Resolve b_errors", Scope: "", Forced: false, Data: nil},
+		{Name: "Custom/Resolve b_errors", Scope: "OtherTransaction/Go/query", Forced: false, Data: nil},
 		{Name: "Custom/Validation", Scope: "", Forced: false, Data: nil},
 		{Name: "Custom/Validation", Scope: "OtherTransaction/Go/query", Forced: false, Data: nil},
 		{Name: "OtherTransaction/Go/query", Scope: "", Forced: true, Data: nil},
