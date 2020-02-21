86ddc6a8ec752af1b5d18ac4261290c35c9cbf3e
will@newrelic.com <will@newrelic.com>
2020-02-21 14:03:31 -0800

    Consolidate browser code into one file

--- v3/newrelic/browser_header_test.go
+++ v3/newrelic/browser_header_test.go
@@ -63,3 +63,26 @@ func TestEnabled(t *testing.T) {
 		t.Errorf("unexpected WithoutTags output: expected %s; got %s", expected, string(actual))
 	}
 }
+
+func TestBrowserAttributesNil(t *testing.T) {
+	expected := `{"u":{},"a":{}}`
+	actual := string(browserAttributes(nil))
+	if expected != actual {
+		t.Errorf("unexpected browser attributes: expected %s; got %s", expected, actual)
+	}
+}
+
+func TestBrowserAttributes(t *testing.T) {
+	config := config{Config: defaultConfig()}
+	config.BrowserMonitoring.Attributes.Enabled = true
+	a := newAttributes(createAttributeConfig(config, true))
+	addUserAttribute(a, "user", "thing", destBrowser)
+	addUserAttribute(a, "not", "shown", destError)
+	a.Agent.Add(AttributeHostDisplayName, "host", nil)
+
+	expected := `{"u":{"user":"thing"},"a":{}}`
+	actual := string(browserAttributes(a))
+	if expected != actual {
+		t.Errorf("unexpected browser attributes: expected %s; got %s", expected, actual)
+	}
+}
