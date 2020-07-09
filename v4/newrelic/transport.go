2e09038df485716e7fe89c6b239ef29750ca0491
Rey Abolofia <rabolofia@newrelic.com>
2020-07-09 16:27:27 -0700

    Remove more unused code.

--- v4/newrelic/transport.go
+++ v4/newrelic/transport.go
@@ -1,4 +1,4 @@
-// Copyright 2020 New Relic Corporation. All rights reserved.
+// Copyright 2021 New Relic Corporation. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
 // +build go1.13
