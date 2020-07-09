118adb2786dd01ed56d64248ef0d398a7a5a0e0a
Rey Abolofia <rabolofia@newrelic.com>
2020-07-09 11:36:23 -0700

    Noop instrumentation shims.

--- v4/newrelic/instrumentation.go
+++ v4/newrelic/instrumentation.go
@@ -29,20 +29,7 @@ import (
 //
 // The WrapHandle function is safe to call if app is nil.
 func WrapHandle(app *Application, pattern string, handler http.Handler) (string, http.Handler) {
-	if app == nil {
-		return pattern, handler
-	}
-	return pattern, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		txn := app.StartTransaction(r.Method + " " + pattern)
-		defer txn.End()
-
-		w = txn.SetWebResponse(w)
-		txn.SetWebRequestHTTP(r)
-
-		r = RequestWithTransactionContext(r, txn)
-
-		handler.ServeHTTP(w, r)
-	})
+	return pattern, handler
 }
 
 // WrapHandleFunc instruments handler functions using Transactions.  To
@@ -69,8 +56,7 @@ func WrapHandle(app *Application, pattern string, handler http.Handler) (string,
 //
 // The WrapHandleFunc function is safe to call if app is nil.
 func WrapHandleFunc(app *Application, pattern string, handler func(http.ResponseWriter, *http.Request)) (string, func(http.ResponseWriter, *http.Request)) {
-	p, h := WrapHandle(app, pattern, http.HandlerFunc(handler))
-	return p, func(w http.ResponseWriter, r *http.Request) { h.ServeHTTP(w, r) }
+	return pattern, handler
 }
 
 // NewRoundTripper creates an http.RoundTripper to instrument external requests
@@ -80,37 +66,5 @@ func WrapHandleFunc(app *Application, pattern string, handler func(http.Response
 // http.RoundTripper will look for a Transaction in the request's context
 // (using FromContext).
 func NewRoundTripper(original http.RoundTripper) http.RoundTripper {
-	if nil == original {
-		original = http.DefaultTransport
-	}
-	return roundTripperFunc(func(request *http.Request) (*http.Response, error) {
-		// The specification of http.RoundTripper requires that the request is never modified.
-		request = cloneRequest(request)
-		segment := StartExternalSegment(nil, request)
-
-		response, err := original.RoundTrip(request)
-
-		segment.Response = response
-		segment.End()
-
-		return response, err
-	})
+	return original
 }
-
-// cloneRequest mimics implementation of
-// https://godoc.org/github.com/google/go-github/github#BasicAuthTransport.RoundTrip
-func cloneRequest(r *http.Request) *http.Request {
-	// shallow copy of the struct
-	r2 := new(http.Request)
-	*r2 = *r
-	// deep copy of the Header
-	r2.Header = make(http.Header, len(r.Header))
-	for k, s := range r.Header {
-		r2.Header[k] = append([]string(nil), s...)
-	}
-	return r2
-}
-
-type roundTripperFunc func(*http.Request) (*http.Response, error)
-
-func (f roundTripperFunc) RoundTrip(r *http.Request) (*http.Response, error) { return f(r) }
