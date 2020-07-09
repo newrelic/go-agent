2af810d89a50d4434b791dbc68b1fe1fc8cf4a00
Rey Abolofia <rabolofia@newrelic.com>
2020-07-09 15:23:55 -0700

    Noop some loggers.

--- v4/newrelic/log.go
+++ v4/newrelic/log.go
@@ -3,12 +3,6 @@
 
 package newrelic
 
-import (
-	"io"
-
-	"github.com/newrelic/go-agent/v3/internal/logger"
-)
-
 // Logger is the interface that is used for logging in the Go Agent.  Assign
 // the Config.Logger field to the Logger you wish to use.  Loggers must be safe
 // for use in multiple goroutines.  Two Logger implementations are included:
@@ -24,20 +18,4 @@ type Logger interface {
 	Info(msg string, context map[string]interface{})
 	Debug(msg string, context map[string]interface{})
 	DebugEnabled() bool
-}
-
-// NewLogger creates a basic Logger at info level.
-//
-// Deprecated: NewLogger is deprecated and will be removed in a future release.
-// Use the ConfigInfoLogger ConfigOption instead.
-func NewLogger(w io.Writer) Logger {
-	return logger.New(w, false)
-}
-
-// NewDebugLogger creates a basic Logger at debug level.
-//
-// Deprecated: NewDebugLogger is deprecated and will be removed in a future
-// release.  Use the ConfigDebugLogger ConfigOption instead.
-func NewDebugLogger(w io.Writer) Logger {
-	return logger.New(w, true)
 }
