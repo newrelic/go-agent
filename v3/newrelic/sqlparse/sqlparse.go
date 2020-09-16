75ca87213867f31b7a7adfcb3ffd2d815df5a29c
Johannes Tax <jtax@newrelic.com>
2020-09-16 15:53:36 -0700

    Fixes

--- v3/newrelic/sqlparse/sqlparse.go
+++ v3/newrelic/sqlparse/sqlparse.go
@@ -4,50 +4,80 @@
 package sqlparse
 
 import (
+	"bytes"
 	"fmt"
-	"regexp"
 	"strings"
 	"unicode"
 
 	newrelic "github.com/newrelic/go-agent/v3/newrelic"
 )
 
+var (
+	operations = map[string]string{
+		"select":   "from",
+		"delete":   "from",
+		"insert":   "into",
+		"update":   "",
+		"call":     "",
+		"create":   "",
+		"drop":     "",
+		"show":     "",
+		"set":      "",
+		"exec":     "",
+		"execute":  "",
+		"alter":    "",
+		"commit":   "",
+		"rollback": "",
+	}
+	updateModifiers = map[string]interface{}{
+		"low_priority": nil,
+		"ignore":       nil,
+		"or":           nil,
+		"rollback":     nil,
+		"abort":        nil,
+		"replace":      nil,
+		"fail":         nil,
+		"only":         nil,
+	}
+)
+
+// Extracts the table name from the given string.
 func extractTable(s string) string {
-	s = extractTableRegex.ReplaceAllString(s, "")
 	if idx := strings.Index(s, "."); idx > 0 {
 		s = s[idx+1:]
 	}
-	return s
+
+	var buffer bytes.Buffer
+
+	for _, c := range s {
+		if unicode.IsSpace(c) || strings.ContainsRune("`'(){}[]\"", c) {
+			continue
+		} else {
+			buffer.WriteRune(c)
+		}
+	}
+
+	return buffer.String()
 }
 
-var (
-	basicTable        = `[^)(\]\[\}\{\s,;]+`
-	enclosedTable     = `[\[\(\{]` + `\s*` + basicTable + `\s*` + `[\]\)\}]`
-	tablePattern      = `(` + `\s+` + basicTable + `|` + `\s*` + enclosedTable + `)`
-	extractTableRegex = regexp.MustCompile(`[\s` + "`" + `"'\(\)\{\}\[\]]*`)
-	updateRegex       = regexp.MustCompile(`(?is)^update(?:\s+(?:low_priority|ignore|or|rollback|abort|replace|fail|only))*` + tablePattern)
-	sqlOperations     = map[string]*regexp.Regexp{
-		"select":   regexp.MustCompile(tablePattern),
-		"delete":   regexp.MustCompile(tablePattern),
-		"insert":   regexp.MustCompile(tablePattern),
-		"update":   updateRegex,
-		"call":     nil,
-		"create":   nil,
-		"drop":     nil,
-		"show":     nil,
-		"set":      nil,
-		"exec":     nil,
-		"execute":  nil,
-		"alter":    nil,
-		"commit":   nil,
-		"rollback": nil,
+// Returns a new string with trailing comments removed.
+func cutComment(query string) string {
+	query = skipSpace(query)
+	for i, c := range query {
+		if strings.HasPrefix(query[i:], "/*") || strings.HasPrefix(query[i:], "--") || c == ';' || c == '#' {
+			query = fmt.Sprintf("%s%s", query[:i], skipComment(query[i:]))
+			return cutComment(query)
+		}
 	}
-	firstWordRegex   = regexp.MustCompile(`^\w+`)
-	cCommentRegex    = regexp.MustCompile(`(?is)/\*.*?\*/`)
-	lineCommentRegex = regexp.MustCompile(`(?im)(?:--|#).*?$`)
-	sqlPrefixRegex   = regexp.MustCompile(`^[\s;]*`)
-)
+	return query
+}
 
+type sqlQuery struct {
+	q string
+	p uint64
+}
+
+// Returns a string slice of query without trailing spaces.
 func skipSpace(query string) string {
 	for i, c := range query {
 		if !unicode.IsSpace(c) {
@@ -57,6 +87,7 @@ func skipSpace(query string) string {
 	return ""
 }
 
+// Returns a string slice of query with trailing comments removed.
 func skipComment(query string) string {
 	query = skipSpace(query)
 
@@ -86,45 +117,44 @@ func skipComment(query string) string {
 	return query
 }
 
-func cutComment(query string) string {
-	query = skipSpace(query)
-	for i, _ := range query {
-		if strings.HasPrefix(query[i:], "/*") || strings.HasPrefix(query[i:], "--") || strings.HasPrefix(query[i:], ";") {
-			query = fmt.Sprintf("%s%s", query[:i], skipComment(query[i:]))
-			return cutComment(query)
-		}
-	}
-	return query
-}
-
+// Returns the first word and the remainder of the given string (with trailing
+// comments removed).
+//
+// A word is either sequence of letters and digits, or a single special
+// character.
 func firstWord(query string) (string, string) {
 	query = skipSpace(query)
 	for i, c := range query {
 		if !unicode.IsLetter(c) && !unicode.IsDigit(c) {
-			if unicode.IsSpace(c) || c == '(' || c == '[' || c == '{' {
-				return query[0:i], skipSpace(query[i+1:])
+			if unicode.IsSpace(c) || i != 0 {
+				return query[0:i], skipComment(query[i+1:])
 			} else {
-				return query[0 : i+1], skipSpace(query[i+1:])
+				return query[0 : i+1], skipComment(query[i+1:])
 			}
 		}
 	}
 	return query, ""
 }
 
-func firstWordOnlySpace(query string) (string, string) {
+// Returns the first token and the remainder of the given string (with trailing
+// comments removed).
+//
+// A token is either an expression surrounded by [], or the charater sequence
+// before the first space, '(', or '{'.
+func firstToken(query string) (string, string) {
 	query = skipSpace(query)
 
 	if strings.HasPrefix(query, "[") {
 		for i, c := range query {
 			if c == ']' {
-				return query[0 : i+1], skipSpace(query[i+1:])
+				return query[0 : i+1], skipComment(query[i+1:])
 			}
 		}
 	}
 
 	for i, c := range query {
-		if unicode.IsSpace(c) || (i != 0 && (c == '(' || c == ',' || c == '{')) {
-			return query[0:i], skipSpace(query[i+1:])
+		if unicode.IsSpace(c) || (i != 0 && strings.ContainsRune("({,", c)) {
+			return query[0:i], skipComment(query[i+1:])
 		}
 	}
 	return query, ""
@@ -139,78 +169,36 @@ func firstWordOnlySpace(query string) (string, string) {
 // Ability to correctly parse queries for other SQL databases is not
 // guaranteed.
 func ParseQuery(segment *newrelic.DatastoreSegment, query string) {
-	//fmt.Printf("### Original       : %v\n", query)
 	s := skipComment(query)
-	//fmt.Printf("### Skipped comment: %v\n", s)
 	op, s := firstWord(s)
-	op = strings.ToLower(op)
-	//fmt.Printf("### First word     : %v.\n", op)
-	fmt.Printf("")
-	op = cutComment(op)
-	if _, ok := sqlOperations[op]; ok {
+	op = strings.ToLower(cutComment(op))
+	if tablePrefix, ok := operations[op]; ok {
 		segment.Operation = op
-		if op == "select" || op == "delete" || op == "insert" {
-			var token string
-			if op == "insert" {
-				token = "into"
-			} else {
-				token = "from"
-			}
+		if tablePrefix != "" {
 			for {
-				s = skipComment(s)
-				from, next := firstWord(s)
-				next = skipComment(next)
-				s = next
-				//fmt.Printf("### query: %v, first word %v\n", query, from)
-				if strings.ToLower(from) == token {
-					table, next := firstWordOnlySpace(s)
-					//fmt.Printf("### s: %v\n", s)
-					//fmt.Printf("### table: %v, next: %v\n", table, next)
-					s = next
+				var token string
+				token, s = firstWord(s)
+				if token == "" {
+					break
+				}
+				if strings.ToLower(token) == tablePrefix {
+					var table string
+					table, s = firstToken(s)
 					segment.Collection = extractTable(cutComment(table))
-				} else if from == "" {
-					break
-				}
-			}
-		} else if op == "update" {
-			for {
-				modifiers := map[string]interface{}{
-					"low_priority": nil,
-					"ignore":       nil,
-					"or":           nil,
-					"rollback":     nil,
-					"abort":        nil,
-					"replace":      nil,
-					"fail":         nil,
-					"only":         nil,
-				}
-
-				s = skipComment(s)
-				from, next := firstWordOnlySpace(s)
-				next = skipComment(next)
-				s = next
-				//fmt.Printf("### query: %v, first word %v\n", query, from)
-				if _, ok := modifiers[strings.ToLower(from)]; ok {
-					continue
-				} else {
-					segment.Collection = extractTable(cutComment(from))
-					break
 				}
 			}
 		}
-	}
-}
-
-func ParseQuery2(segment *newrelic.DatastoreSegment, query string) {
-	s := cCommentRegex.ReplaceAllString(query, "")
-	s = lineCommentRegex.ReplaceAllString(s, "")
-	s = sqlPrefixRegex.ReplaceAllString(s, "")
-	op := strings.ToLower(firstWordRegex.FindString(query))
-	if rg, ok := sqlOperations[op]; ok {
-		segment.Operation = op
-		if nil != rg {
-			if m := rg.FindStringSubmatch(query); len(m) > 1 {
-				segment.Collection = extractTable(m[1])
+		if op == "update" {
+			for {
+				var token string
+				token, s = firstToken(s)
+				if token == "" {
+					break
+				}
+				if _, ok := updateModifiers[strings.ToLower(token)]; !ok {
+					segment.Collection = extractTable(cutComment(token))
+					break
+				}
 			}
 		}
 	}
