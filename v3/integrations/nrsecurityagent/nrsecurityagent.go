9ca2ab873baaab55f6aa9d0444ddae5cf55b82be
Aayush garg <agarg@newrelic.com>
2024-09-18 09:53:53 +0530

    update config tags

--- v3/integrations/nrsecurityagent/nrsecurityagent.go
+++ v3/integrations/nrsecurityagent/nrsecurityagent.go
@@ -31,10 +31,10 @@ func defaultSecurityConfig() SecurityConfig {
 	cfg.Security.Agent.Enabled = true
 	cfg.Security.Detection.Rxss.Enabled = true
 	cfg.Security.Request.BodyLimit = 300
-	cfg.Security.SkipIastScan.Parameters.Header = make([]string, 0)
-	cfg.Security.SkipIastScan.Parameters.Body = make([]string, 0)
-	cfg.Security.SkipIastScan.Parameters.Query = make([]string, 0)
-	cfg.Security.SkipIastScan.API = make([]string, 0)
+	cfg.Security.ExcludeFromIastScan.HttpRequestParameters.Header = make([]string, 0)
+	cfg.Security.ExcludeFromIastScan.HttpRequestParameters.Body = make([]string, 0)
+	cfg.Security.ExcludeFromIastScan.HttpRequestParameters.Query = make([]string, 0)
+	cfg.Security.ExcludeFromIastScan.API = make([]string, 0)
 	return cfg
 }
 
