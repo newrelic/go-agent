16c44e5e2b55bad9010d65d91dff1929297b3c15
mirackara <mirackara2000@outlook.com>
2025-04-16 10:21:16 -0500

    skip codecov upload on arm tests

--- run-tests.sh
+++ run-tests.sh
@@ -34,11 +34,17 @@ verify_go_fmt
 sed -i '/sql_driver_optional_methods/d' "$COVERAGE_FILE"
 
 ## CodeCov Uploader
-curl https://keybase.io/codecovsecurity/pgp_keys.asc | gpg --no-default-keyring --import # One-time step
-curl -Os https://uploader.codecov.io/latest/linux/codecov
-curl -Os https://uploader.codecov.io/latest/linux/codecov.SHA256SUM
-curl -Os https://uploader.codecov.io/latest/linux/codecov.SHA256SUM.sig
-gpg --verify codecov.SHA256SUM.sig codecov.SHA256SUM
-shasum -a 256 -c codecov.SHA256SUM
-chmod +x codecov
-./codecov -t ${CODECOV_TOKEN} -f "$COVERAGE_FILE" -B "${GITHUB_HEAD_REF:-$GITHUB_REF}"
+if [ -n "$CODECOV_TOKEN" ]; then
+  echo "Codecov token found; attempting to upload coverage report."
+
+  curl https://keybase.io/codecovsecurity/pgp_keys.asc | gpg --no-default-keyring --import # One-time step
+  curl -Os https://uploader.codecov.io/latest/linux/codecov
+  curl -Os https://uploader.codecov.io/latest/linux/codecov.SHA256SUM
+  curl -Os https://uploader.codecov.io/latest/linux/codecov.SHA256SUM.sig
+  gpg --verify codecov.SHA256SUM.sig codecov.SHA256SUM
+  shasum -a 256 -c codecov.SHA256SUM
+  chmod +x codecov
+  ./codecov -t ${CODECOV_TOKEN} -f "$COVERAGE_FILE" -B "${GITHUB_HEAD_REF:-$GITHUB_REF}"
+else
+  echo "Codecov token is not set. Skipping Codecov upload."
+fi
