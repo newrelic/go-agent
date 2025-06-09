4695bd9da79d7bd07a1fca48e3c09ab878da60a8
mvick <mvick@newrelic.com>
2025-06-09 17:47:50 -0400

    test: Updates to examples

--- v3/newrelic/integrationsupport/example_test.go
+++ v3/newrelic/integrationsupport/example_test.go
@@ -4,9 +4,12 @@
 package integrationsupport
 
 import (
+	"context"
 	"github.com/newrelic/go-agent/v3/internal"
 	"github.com/newrelic/go-agent/v3/newrelic"
+	"net/http"
 	"testing"
+	"time"
 )
 
 type myError struct{}
@@ -15,7 +18,7 @@ func (e myError) Error() string { return "My error message" }
 
 func TestNewBasicTestApp(t *testing.T) {
 	expectedApp := NewBasicTestApp()
-	txn := expectedApp.Application.StartTransaction("My transaction")
+	txn := expectedApp.Application.StartTransaction("MyTransaction")
 	txn.NoticeError(myError{})
 	txn.End()
 	expectedApp.ExpectErrors(t, []internal.WantError{{
@@ -24,16 +27,48 @@ func TestNewBasicTestApp(t *testing.T) {
 }
 
 func TestDistributedTracingTestApp(t *testing.T) {
-	expectedApp := NewTestApp(SampleEverythingReplyFn, DTEnabledCfgFn)
-	txn := expectedApp.Application.StartTransaction("My transaction")
-	defer txn.End()
 
-	// will add more here
+	expectedApp := NewTestApp(SampleEverythingReplyFn, DTEnabledCfgFn)
+	txn := expectedApp.Application.StartTransaction("MyTransaction")
+
+	client := &http.Client{}
+	client.Transport = newrelic.NewRoundTripper(client.Transport)
+
+	request, _ := http.NewRequest("GET", "https://example.com", nil)
+
+	request = request.WithContext(newrelic.NewContext(context.Background(), txn))
+
+	_, err := client.Do(request)
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+
+	time.Sleep(2 * time.Second) // Sleep to exceed trace threshold
+	txn.End()
+
+	expectedApp.ExpectMetrics(t, []internal.WantMetric{
+		{Name: "DurationByCaller/Unknown/Unknown/Unknown/Unknown/all", Scope: "", Forced: false, Data: nil},
+		{Name: "DurationByCaller/Unknown/Unknown/Unknown/Unknown/allOther", Scope: "", Forced: false, Data: nil},
+		{Name: "OtherTransaction/Go/MyTransaction", Scope: "", Forced: true, Data: nil},
+		{Name: "OtherTransaction/all", Scope: "", Forced: true, Data: nil},
+		{Name: "OtherTransactionTotalTime", Scope: "", Forced: true, Data: nil},
+		{Name: "OtherTransactionTotalTime/Go/MyTransaction", Scope: "", Forced: false, Data: nil},
+		{Name: "External/all", Scope: "", Forced: true, Data: nil},
+		{Name: "External/allOther", Scope: "", Forced: true, Data: nil},
+		{Name: "External/example.com/all", Scope: "", Forced: false, Data: nil},
+		{Name: "External/example.com/http/GET", Scope: "OtherTransaction/Go/MyTransaction", Forced: false, Data: nil},
+	})
+
+	expectedApp.ExpectTxnTraces(t, []internal.WantTxnTrace{{
+		MetricName:  "OtherTransaction/Go/MyTransaction",
+		NumSegments: 1,
+	}})
 }
 
 func TestAppLogsTestApp(t *testing.T) {
 	expectedApp := NewTestApp(SampleEverythingReplyFn, AppLogEnabledCfgFn)
-	txn := expectedApp.Application.StartTransaction("My transaction")
+	txn := expectedApp.Application.StartTransaction("MyTransaction")
 	defer txn.End()
 
 	txn.RecordLog(newrelic.LogData{
@@ -65,7 +100,7 @@ func TestAppLogsTestApp(t *testing.T) {
 
 func TestFullTracesTestApp(t *testing.T) {
 	expectedApp := NewTestApp(SampleEverythingReplyFn, ConfigFullTraces, newrelic.ConfigCodeLevelMetricsEnabled(false))
-	txn := expectedApp.Application.StartTransaction("test")
+	txn := expectedApp.Application.StartTransaction("MyTransaction")
 
 	AddAgentAttribute(txn, newrelic.AttributeSpanKind, "producer", nil)
 
