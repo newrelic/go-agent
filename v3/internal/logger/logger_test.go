8f16ebe89a33d8fbef924b3ad38d6fe964325cbc
Yaser Ahmed <yahmed@newrelic.com>
2025-07-02 11:31:58 -0500

    compare jsonStr and expected

--- v3/internal/logger/logger_test.go
+++ v3/internal/logger/logger_test.go
@@ -38,14 +38,24 @@ func TestBasicLogger(t *testing.T) {
 		t.Error("Debug logging is not enabled")
 	}
 
+	var expected []string
+	expected = append(expected,
+		"{\"level\":\"error\",\"msg\":\"error message\",\"context\":{\"key1\":\"val1\",\"key2\":\"val2\"}}",
+		"{\"level\":\"warn\",\"msg\":\"warn message\",\"context\":{\"key1\":\"val1\",\"key2\":\"val2\"}}",
+		"{\"level\":\"info\",\"msg\":\"info message\",\"context\":{\"key1\":\"val1\",\"key2\":\"val2\"}}",
+		"{\"level\":\"debug\",\"msg\":\"debug message\",\"context\":{\"key1\":\"val1\",\"key2\":\"val2\"}}")
+
 	var jsonMap map[string]interface{}
 	s := strings.Split(b.String(), "\n")
 	s = s[:len(s)-1]
-	for _, v := range s {
+	for i, v := range s {
 		jsonStr := v[strings.Index(v, "{"):]
 		err := json.Unmarshal([]byte(jsonStr), &jsonMap)
 		if err != nil {
 			t.Errorf("Error %v unmarshaling JSON:", err)
+		}
+		if jsonStr != expected[i] {
+			t.Errorf("JSON string does not match expected:\n\tExpected: %v\n\tActual: %v", expected[i], jsonStr)
 		}
 	}
 }
