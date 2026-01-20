9b1d3a761837aa62b67bc82d2b253459898d00f4
Cade Conklin <conklincade@gmail.com>
2026-01-20 14:25:46 -0800

    fix: No more request and response wrappers

--- v3/integrations/nroci/nrocinosql_ocigosdk.go
+++ v3/integrations/nroci/nrocinosql_ocigosdk.go
@@ -28,14 +28,6 @@ type NoSQLClientWrapper struct {
 	Client NoSQLClient
 }
 
-type NoSQLClientRequestWrapper[R any] struct {
-	ClientRequest R
-}
-
-type NoSQLClientResponseWrapper[T any] struct {
-	ClientResponse T
-}
-
 func NRNewNoSQLClientWithConfigurationProvider(configProvider common.ConfigurationProvider) (*NoSQLClientWrapper, error) {
 	ociNoSQLClient, err := nosql.NewNosqlClientWithConfigurationProvider(configProvider)
 	if err != nil {
@@ -92,17 +84,17 @@ func extractRequestFieldsOCI(req any) (string, string, string) {
 
 func executeWithDatastoreSegmentOCI[T any, R any](
 	ctx context.Context,
-	rw *NoSQLClientRequestWrapper[R],
+	req R,
 	fn func() (T, error),
 	operation string,
-) (*NoSQLClientResponseWrapper[T], error) {
+) (*T, error) {
 
 	txn := newrelic.FromContext(ctx)
 	if txn == nil {
 		return nil, fmt.Errorf("error executing OCI request, no transaction")
 	}
 
-	collection, statement, compartmentID := extractRequestFieldsOCI(rw.ClientRequest)
+	collection, statement, compartmentID := extractRequestFieldsOCI(req)
 	sgmt := newrelic.DatastoreSegment{
 		StartTime:          txn.StartSegmentNow(),
 		Product:            newrelic.DatastoreOracle,
@@ -115,74 +107,56 @@ func executeWithDatastoreSegmentOCI[T any, R any](
 	if err != nil {
 		return nil, fmt.Errorf("error executing OCI requestL %s", err.Error())
 	}
-	responseWrapper := NoSQLClientResponseWrapper[T]{
-		ClientResponse: res,
-	}
 
+	var response T = res
 	sgmt.End()
-	return &responseWrapper, nil
+	return &response, nil
 }
 
-func (cw *NoSQLClientWrapper) Query(ctx context.Context, req nosql.QueryRequest) (*NoSQLClientResponseWrapper[nosql.QueryResponse], error) {
-	return executeWithDatastoreSegmentOCI(ctx, &NoSQLClientRequestWrapper[nosql.QueryRequest]{
-		ClientRequest: req,
-	}, func() (nosql.QueryResponse, error) {
+func (cw *NoSQLClientWrapper) Query(ctx context.Context, req nosql.QueryRequest) (*nosql.QueryResponse, error) {
+	return executeWithDatastoreSegmentOCI(ctx, req, func() (nosql.QueryResponse, error) {
 		return cw.Client.Query(ctx, req)
 	}, "Query")
 }
 
-func (cw *NoSQLClientWrapper) UpdateRow(ctx context.Context, req nosql.UpdateRowRequest) (*NoSQLClientResponseWrapper[nosql.UpdateRowResponse], error) {
-	return executeWithDatastoreSegmentOCI(ctx, &NoSQLClientRequestWrapper[nosql.UpdateRowRequest]{
-		ClientRequest: req,
-	}, func() (nosql.UpdateRowResponse, error) {
+func (cw *NoSQLClientWrapper) UpdateRow(ctx context.Context, req nosql.UpdateRowRequest) (*nosql.UpdateRowResponse, error) {
+	return executeWithDatastoreSegmentOCI(ctx, req, func() (nosql.UpdateRowResponse, error) {
 		return cw.Client.UpdateRow(ctx, req)
 	}, "UpdateRow")
 }
 
-func (cw *NoSQLClientWrapper) CreateTable(ctx context.Context, req nosql.CreateTableRequest) (*NoSQLClientResponseWrapper[nosql.CreateTableResponse], error) {
-	return executeWithDatastoreSegmentOCI(ctx, &NoSQLClientRequestWrapper[nosql.CreateTableRequest]{
-		ClientRequest: req,
-	}, func() (nosql.CreateTableResponse, error) {
+func (cw *NoSQLClientWrapper) CreateTable(ctx context.Context, req nosql.CreateTableRequest) (*nosql.CreateTableResponse, error) {
+	return executeWithDatastoreSegmentOCI(ctx, req, func() (nosql.CreateTableResponse, error) {
 		return cw.Client.CreateTable(ctx, req)
 	}, "CreateTable")
 }
 
-func (cw *NoSQLClientWrapper) DeleteRow(ctx context.Context, req nosql.DeleteRowRequest) (*NoSQLClientResponseWrapper[nosql.DeleteRowResponse], error) {
-	return executeWithDatastoreSegmentOCI(ctx, &NoSQLClientRequestWrapper[nosql.DeleteRowRequest]{
-		ClientRequest: req,
-	}, func() (nosql.DeleteRowResponse, error) {
+func (cw *NoSQLClientWrapper) DeleteRow(ctx context.Context, req nosql.DeleteRowRequest) (*nosql.DeleteRowResponse, error) {
+	return executeWithDatastoreSegmentOCI(ctx, req, func() (nosql.DeleteRowResponse, error) {
 		return cw.Client.DeleteRow(ctx, req)
 	}, "DeleteRow")
 }
 
-func (cw *NoSQLClientWrapper) DeleteTable(ctx context.Context, req nosql.DeleteTableRequest) (*NoSQLClientResponseWrapper[nosql.DeleteTableResponse], error) {
-	return executeWithDatastoreSegmentOCI(ctx, &NoSQLClientRequestWrapper[nosql.DeleteTableRequest]{
-		ClientRequest: req,
-	}, func() (nosql.DeleteTableResponse, error) {
+func (cw *NoSQLClientWrapper) DeleteTable(ctx context.Context, req nosql.DeleteTableRequest) (*nosql.DeleteTableResponse, error) {
+	return executeWithDatastoreSegmentOCI(ctx, req, func() (nosql.DeleteTableResponse, error) {
 		return cw.Client.DeleteTable(ctx, req)
 	}, "DeleteTable")
 }
 
-func (cw *NoSQLClientWrapper) GetRow(ctx context.Context, req nosql.GetRowRequest) (*NoSQLClientResponseWrapper[nosql.GetRowResponse], error) {
-	return executeWithDatastoreSegmentOCI(ctx, &NoSQLClientRequestWrapper[nosql.GetRowRequest]{
-		ClientRequest: req,
-	}, func() (nosql.GetRowResponse, error) {
+func (cw *NoSQLClientWrapper) GetRow(ctx context.Context, req nosql.GetRowRequest) (*nosql.GetRowResponse, error) {
+	return executeWithDatastoreSegmentOCI(ctx, req, func() (nosql.GetRowResponse, error) {
 		return cw.Client.GetRow(ctx, req)
 	}, "GetRow")
 }
 
-func (cw *NoSQLClientWrapper) GetTable(ctx context.Context, req nosql.GetTableRequest) (*NoSQLClientResponseWrapper[nosql.GetTableResponse], error) {
-	return executeWithDatastoreSegmentOCI(ctx, &NoSQLClientRequestWrapper[nosql.GetTableRequest]{
-		ClientRequest: req,
-	}, func() (nosql.GetTableResponse, error) {
+func (cw *NoSQLClientWrapper) GetTable(ctx context.Context, req nosql.GetTableRequest) (*nosql.GetTableResponse, error) {
+	return executeWithDatastoreSegmentOCI(ctx, req, func() (nosql.GetTableResponse, error) {
 		return cw.Client.GetTable(ctx, req)
 	}, "GetTable")
 }
 
-func (cw *NoSQLClientWrapper) UpdateTable(ctx context.Context, req nosql.UpdateTableRequest) (*NoSQLClientResponseWrapper[nosql.UpdateTableResponse], error) {
-	return executeWithDatastoreSegmentOCI(ctx, &NoSQLClientRequestWrapper[nosql.UpdateTableRequest]{
-		ClientRequest: req,
-	}, func() (nosql.UpdateTableResponse, error) {
+func (cw *NoSQLClientWrapper) UpdateTable(ctx context.Context, req nosql.UpdateTableRequest) (*nosql.UpdateTableResponse, error) {
+	return executeWithDatastoreSegmentOCI(ctx, req, func() (nosql.UpdateTableResponse, error) {
 		return cw.Client.UpdateTable(ctx, req)
 	}, "UpdateTable")
 }
