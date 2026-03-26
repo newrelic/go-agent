78cbbcaed5ff1f18eca54892d58a6ed4ee10415d
Cade Conklin <conklincade@gmail.com>
2026-03-26 13:10:00 -0700

    feat(nrgocqlx): Wrap session and query

--- v3/integrations/nrgocqlx/nrgocqlx.go
+++ v3/integrations/nrgocqlx/nrgocqlx.go
@@ -11,6 +11,7 @@ import (
 	gocql "github.com/gocql/gocql"
 	"github.com/newrelic/go-agent/v3/internal"
 	"github.com/newrelic/go-agent/v3/newrelic"
+	"github.com/scylladb/gocqlx/v3"
 )
 
 func init() { internal.TrackUsage("integration", "datastore", "gocql") }
@@ -19,6 +20,81 @@ type queryObserver struct {
 	original interface {
 		ObserveQuery(ctx context.Context, query gocql.ObservedQuery)
 	}
+}
+
+type NRGocqlxSessionWrapper struct {
+	*gocqlx.Session
+}
+
+type NRGocqlxQueryxWrapper struct {
+	*gocqlx.Queryx
+}
+
+func NRGoCQLXWrapSession(cluster *gocql.ClusterConfig) (*NRGocqlxSessionWrapper, error) {
+	session, err := gocqlx.WrapSession(cluster.CreateSession())
+	if err != nil {
+		return nil, err
+	}
+	return &NRGocqlxSessionWrapper{&session}, nil
+}
+
+func execOriginal(ctx context.Context, fn func(ctx context.Context, dest any) error, dest any) error {
+	txn := newrelic.FromContext(ctx)
+	if txn == nil {
+		return fn(ctx, dest)
+	}
+
+	// start datastore segment
+	sgmt := &newrelic.DatastoreSegment{
+		StartTime: txn.StartSegmentNow(),
+		Product:   newrelic.DatastoreCassandra,
+	}
+	defer sgmt.End()
+
+	// securtiy agent?
+	ctx = context.WithValue(ctx, "nrGocqlxSegment", sgmt)
+	return fn(ctx, dest) // enriching of sgmt called withing fn()
+}
+
+func (s *NRGocqlxSessionWrapper) ContextQuery(ctx context.Context, stmt string, names []string) *NRGocqlxQueryxWrapper {
+	return &NRGocqlxQueryxWrapper{s.Session.ContextQuery(ctx, stmt, names)}
+}
+
+func (x *NRGocqlxQueryxWrapper) Bind(v ...any) *NRGocqlxQueryxWrapper {
+	return &NRGocqlxQueryxWrapper{x.Queryx.Bind(v...)}
+}
+
+func (x *NRGocqlxQueryxWrapper) BindMap(arg map[string]any) *NRGocqlxQueryxWrapper {
+	return &NRGocqlxQueryxWrapper{x.Queryx.BindMap(arg)}
+}
+
+func (x *NRGocqlxQueryxWrapper) BindStruct(arg any) *NRGocqlxQueryxWrapper {
+	return &NRGocqlxQueryxWrapper{x.Queryx.BindStruct(arg)}
+}
+
+func (x *NRGocqlxQueryxWrapper) BindStructMap(arg0 any, arg1 map[string]any) *NRGocqlxQueryxWrapper {
+	return &NRGocqlxQueryxWrapper{x.Queryx.BindStructMap(arg0, arg1)}
+}
+
+func (x *NRGocqlxQueryxWrapper) SelectRelease(dest any) error {
+	return execOriginal(x.Queryx.Query.Context(), func(ctx context.Context, dest any) error {
+		x.Queryx.Query = x.Queryx.Query.WithContext(ctx) // Update ctx stored in Query with segment
+		return x.Queryx.SelectRelease(dest)
+	}, dest)
+}
+
+func (x *NRGocqlxQueryxWrapper) GetRelease(dest any) error {
+	return execOriginal(x.Queryx.Query.Context(), func(ctx context.Context, dest any) error {
+		x.Queryx.Query = x.Queryx.Query.WithContext(ctx)
+		return x.Queryx.GetRelease(dest)
+	}, dest)
+}
+
+func (x *NRGocqlxQueryxWrapper) ExecRelease() error {
+	return execOriginal(x.Queryx.Query.Context(), func(ctx context.Context, dest any) error {
+		x.Queryx.Query = x.Queryx.Query.WithContext(ctx)
+		return x.Queryx.ExecRelease()
+	}, nil)
 }
 
 // NewQueryObserver returns a gocql.QueryObserver that creates
