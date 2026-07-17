ceb90ef37446fed13e0d049d005ba4c4cf125c9d
Cade Conklin <conklincade@gmail.com>
2026-07-17 11:12:11 -0700

    feat: Outline SpanProcessor interface functions

--- v3/integrations/nrotelhybrid/nrotelhybrid.go
+++ v3/integrations/nrotelhybrid/nrotelhybrid.go
@@ -1,1 +1,26 @@
 package nrotelhybrid
+
+import (
+	"context"
+
+	"go.opentelemetry.io/otel/sdk/trace"
+)
+
+type nrotelhybridProcessor struct {
+}
+
+func (p *nrotelhybridProcessor) OnStart(ctx context.Context, s trace.ReadWriteSpan) {
+
+}
+
+func (p *nrotelhybridProcessor) OnEnd(s trace.ReadOnlySpan) {
+
+}
+
+func (p *nrotelhybridProcessor) Shutdown(ctx context.Context) error {
+
+}
+
+func (p *nrotelhybridProcessor) ForceFlush(ctx context.Context) error {
+
+}
