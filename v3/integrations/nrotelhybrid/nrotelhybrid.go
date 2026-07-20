ff39f3490c595b574141e6e1b3c65ab987e20544
Cade Conklin <conklincade@gmail.com>
2026-07-20 16:14:08 -0700

    feat: Transaction started based on span kind

--- v3/integrations/nrotelhybrid/nrotelhybrid.go
+++ v3/integrations/nrotelhybrid/nrotelhybrid.go
@@ -10,7 +10,10 @@ import (
 
 type nrotelhybridProcessor struct {
 	app    *newrelic.Application
-	txnMap map[oteltrace.TraceID]*newrelic.Transaction // Trace ID -> Transaction
+	txnMap map[oteltrace.TraceID]struct {
+		txn    *newrelic.Transaction
+		spanID oteltrace.SpanID
+	} // Trace ID -> Transaction
 }
 
 func (p *nrotelhybridProcessor) OnStart(ctx context.Context, s trace.ReadWriteSpan) {
@@ -20,19 +23,22 @@ func (p *nrotelhybridProcessor) OnStart(ctx context.Context, s trace.ReadWriteSp
 	// check if remote parent
 	// should be a valid span context and be marked as remote
 	// this begins a transaction
-	if isTransaction(s.Parent()) {
+	if p.isTransaction(s.SpanKind(), s.SpanContext(), s.Parent()) {
 		txn := p.app.StartTransaction(s.Name())
-		p.txnMap[s.SpanContext().TraceID()] = txn
+		p.txnMap[s.SpanContext().TraceID()] = struct {
+			txn    *newrelic.Transaction
+			spanID oteltrace.SpanID
+		}{txn, s.SpanContext().SpanID()}
 	}
 
 }
 
 func (p *nrotelhybridProcessor) OnEnd(s trace.ReadOnlySpan) {
 	// use the trace id from trace.ReadOnlySpan to end the transaction
-	if isTransaction(s.Parent()) {
-		txn := p.txnMap[s.SpanContext().TraceID()]
-		if txn != nil {
-			txn.End()
+	if p.isTransaction(s.SpanKind(), s.SpanContext(), s.Parent()) {
+		if val, ok := p.txnMap[s.SpanContext().TraceID()]; ok && val.txn != nil {
+			val.txn.End()
+			delete(p.txnMap, s.SpanContext().TraceID())
 		}
 	}
 }
@@ -45,6 +51,34 @@ func (p *nrotelhybridProcessor) ForceFlush(ctx context.Context) error {
 	return nil
 }
 
-func isTransaction(parent oteltrace.SpanContext) bool {
-	return !parent.IsValid() || parent.IsRemote()
+func (p *nrotelhybridProcessor) isTransaction(kind oteltrace.SpanKind, current oteltrace.SpanContext, parent oteltrace.SpanContext) bool {
+	if parent.IsRemote() {
+		// if valid remote parent
+		return parent.IsValid()
+	}
+	// check if within a transaction
+	switch kind {
+	case oteltrace.SpanKindUnspecified:
+		return false
+	case oteltrace.SpanKindInternal:
+		return false
+	case oteltrace.SpanKindServer:
+		return !p.isWithinTransaction(current.TraceID(), current.SpanID())
+	case oteltrace.SpanKindClient:
+		return false
+	case oteltrace.SpanKindProducer:
+		return false
+	case oteltrace.SpanKindConsumer:
+		return !p.isWithinTransaction(current.TraceID(), current.SpanID())
+	default:
+		return false
+
+	}
+}
+
+func (p *nrotelhybridProcessor) isWithinTransaction(traceID oteltrace.TraceID, spanID oteltrace.SpanID) bool {
+	if val, ok := p.txnMap[traceID]; ok {
+		return val.spanID != spanID
+	}
+	return false
 }
