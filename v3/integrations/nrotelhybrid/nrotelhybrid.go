265fa25b581e165308023c674ee8734db8ab6f56
Cade Conklin <conklincade@gmail.com>
2026-07-20 10:27:08 -0700

    feat: Check for remote parent

--- v3/integrations/nrotelhybrid/nrotelhybrid.go
+++ v3/integrations/nrotelhybrid/nrotelhybrid.go
@@ -10,6 +10,14 @@ type nrotelhybridProcessor struct {
 }
 
 func (p *nrotelhybridProcessor) OnStart(ctx context.Context, s trace.ReadWriteSpan) {
+	// for now pretending like everything is enabled
+	// first check for case when span has a remote parent.  In this case, it must create a new transaction
+
+	// check if remote parent
+	// should be a valid span context and be marked as remote
+	// this begins a transaction
+	if s.Parent().IsValid() && s.Parent().IsRemote() {
+	}
 
 }
 
@@ -18,9 +26,9 @@ func (p *nrotelhybridProcessor) OnEnd(s trace.ReadOnlySpan) {
 }
 
 func (p *nrotelhybridProcessor) Shutdown(ctx context.Context) error {
-
+	return nil
 }
 
 func (p *nrotelhybridProcessor) ForceFlush(ctx context.Context) error {
-
+	return nil
 }
